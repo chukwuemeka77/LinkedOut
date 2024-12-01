8813f7da5d0154fa8fd91570abe3b5adb24c8dce
Chukwuemeka <101349109+chukwuemeka77@users.noreply.github.com>
2024-12-01 19:12:48 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // Use hashed passwords in production
+  password: { type: String, required: true },
+  plan: { type: mongoose.Schema.Types.ObjectId, ref: 'Plan', default: null }, // User's current plan
+  subscriptionActive: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', userSchema);
