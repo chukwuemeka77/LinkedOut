1dc9f1c7f5c8aaf9fe7d937f0e0a291712998f92
Chukwuemeka <101349109+chukwuemeka77@users.noreply.github.com>
2024-12-01 19:02:08 +0100

    Create links.js

---
+++ models/links.js
@@ -0,0 +1,13 @@
+// models/Link.js
+const mongoose = require('mongoose');
+
+const linkSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  title: { type: String, required: true },
+  url: { type: String, required: true },
+  order: { type: Number, required: true },
+  clicks: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model('Link', linkSchema);
+  
